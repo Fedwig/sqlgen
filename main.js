e5cd326c97556117b550684873145048ef79f8ed
Darrshan Rajenderan <85858497+Fedwig@users.noreply.github.com>
2024-02-08 22:58:41 +0800

    Initial Commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// To change the tabs for Simple, Advance and NSE mode
+function ChangeTab(evt, customTab) {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(customTab).style.display = "block";
+  evt.currentTarget.className += " active";
+}
